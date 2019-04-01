2b56e8d54843592c85ad6d5ae47e50793c7cbae8
BuildTools <mato.dipcar@gmail.com>
2019-04-01 12:36:02 +0200

    Changed MOD-ID

--- src/main/java/sk/psuchtak/money/Reference.java
+++ src/main/java/sk/psuchtak/money/Reference.java
@@ -1,7 +1,7 @@
 package sk.psuchtak.money;
 
 public class Reference {
-	public static final String MODID = "psuchtakmoneymod";
+	public static final String MODID = "dipomoneymod";
 	public static final String MOD_NAME = "Psuchtak's Money Mod";
 	public static final String VERSION = "1.0.0";
 	public static final String CLIENT_PROXY_CLASS = "sk.psuchtak.money.proxy.ClientProxy";
