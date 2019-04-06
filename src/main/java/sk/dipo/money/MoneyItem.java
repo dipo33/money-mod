ce7a0cdea41d7654a48e60716402b21f520a4bb7
BuildTools <mato.dipcar@gmail.com>
2019-04-06 19:39:27 +0200

    Replace all item's creative tab to new one

--- src/main/java/sk/dipo/money/MoneyItem.java
+++ src/main/java/sk/dipo/money/MoneyItem.java
@@ -1,13 +1,12 @@
 package sk.dipo.money;
 
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class MoneyItem extends Item {
 
 	public MoneyItem(String name) {
 		setTextureName(Reference.MODID + ":" + name);
-		setCreativeTab(CreativeTabs.tabMisc);
+		setCreativeTab(MoneyMod.moneyTab);
 		setUnlocalizedName(name);
 	}
 }
