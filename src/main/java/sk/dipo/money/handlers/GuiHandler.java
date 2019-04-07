68c174662e1844dcf5616af0f80d2085016e671d
BuildTools <mato.dipcar@gmail.com>
2019-04-07 18:37:18 +0200

    Simplife GuiHandler

--- src/main/java/sk/dipo/money/handlers/GuiHandler.java
+++ src/main/java/sk/dipo/money/handlers/GuiHandler.java
@@ -2,12 +2,10 @@ package sk.dipo.money.handlers;
 
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import sk.dipo.money.guis.ContainerWallet;
 import sk.dipo.money.guis.GuiWallet;
 import sk.dipo.money.guis.InventoryWallet;
-import sk.dipo.money.items.ItemWallet;
 
 public class GuiHandler implements IGuiHandler {
 
@@ -17,9 +15,7 @@ public class GuiHandler implements IGuiHandler {
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		switch (ID) {
 		case GUI_WALLET:
-			ItemStack stack = player.inventory.getStackInSlot(player.inventory.currentItem);
-			InventoryWallet inv = ItemWallet.readFromNBT(stack);
-			return new ContainerWallet(player.inventory, inv);
+			return new ContainerWallet(player.inventory, new InventoryWallet(player.getHeldItem()));
 		default:
 			return null;
 		}
@@ -29,9 +25,7 @@ public class GuiHandler implements IGuiHandler {
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		switch (ID) {
 		case GUI_WALLET:
-			ItemStack stack = player.inventory.getStackInSlot(player.inventory.currentItem);
-			InventoryWallet inv = ItemWallet.readFromNBT(stack);
-			return new GuiWallet(player.inventory, inv);
+			return new GuiWallet(player.inventory, (InventoryWallet) new InventoryWallet(player.getHeldItem()));
 		default:
 			return null;
 		}
