b93934d0999b555e882af75e28748664a80e1896
BuildTools <mato.dipcar@gmail.com>
2019-04-07 19:58:37 +0200

    Change version

--- src/main/java/sk/dipo/money/utils/Reference.java
+++ src/main/java/sk/dipo/money/utils/Reference.java
@@ -3,7 +3,7 @@ package sk.dipo.money.utils;
 public class Reference {
 	public static final String MODID = "dipomoneymod";
 	public static final String MOD_NAME = "Dipo's Money Mod";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.1.0";
 	public static final String CLIENT_PROXY = "sk.dipo.money.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "sk.dipo.money.proxy.CommonProxy";
 }
