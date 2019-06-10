c6cb98e5bca32c7d36c31799c697b8bc8ccaee0a
Dipo <mato.dipcar@gmail.com>
2019-06-10 22:23:47 +0200

    Replace fixed value with dynamic from YAML

--- src/main/java/sk/dipo/money/network/packet/server/LoginMessage.java
+++ src/main/java/sk/dipo/money/network/packet/server/LoginMessage.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import sk.dipo.money.MoneyMod;
 import sk.dipo.money.network.packet.client.AtmMovingTextMessage;
 
 public class LoginMessage implements IMessage {
@@ -35,7 +36,7 @@ public class LoginMessage implements IMessage {
 
 			NBTTagCompound nbt = player.getHeldItem().getTagCompound();
 			if (nbt.getString("PIN").equalsIgnoreCase(message.PIN))
-				return new AtmMovingTextMessage("msg.atm.welcome", (short) 3, 100);
+				return new AtmMovingTextMessage("msg.atm.welcome", (short) 3, MoneyMod.db.getInteger("Players", player.getUniqueID().toString() + ".Balance"));
 			else
 				return new AtmMovingTextMessage("msg.atm.bad_login", (short) 2);
 		}
