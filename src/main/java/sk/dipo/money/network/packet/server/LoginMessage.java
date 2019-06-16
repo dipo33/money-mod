2bc6329bce181940bf0f08d02031c341f843ff9b
Dipo <mato.dipcar@gmail.com>
2019-06-16 12:56:24 +0200

    Remove unused import

--- src/main/java/sk/dipo/money/network/packet/server/LoginMessage.java
+++ src/main/java/sk/dipo/money/network/packet/server/LoginMessage.java
@@ -5,7 +5,6 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import sk.dipo.money.MoneyMod;
 import sk.dipo.money.network.packet.client.AtmMovingTextMessage;
